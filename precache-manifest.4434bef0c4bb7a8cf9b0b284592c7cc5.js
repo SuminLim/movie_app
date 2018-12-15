6c88bcee032cb169f7d8c16b7226e708a95e8699
SuMin.Lim <jason.lim@frientrip.com>
2018-12-15 16:42:39 +0900

    Updates

---
+++ precache-manifest.4434bef0c4bb7a8cf9b0b284592c7cc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c2265023417c2de434b1",
+    "url": "/movie_app/static/js/main.c2265023.chunk.js"
+  },
+  {
+    "revision": "7c8f41d282a7da190721",
+    "url": "/movie_app/static/js/1.7c8f41d2.chunk.js"
+  },
+  {
+    "revision": "c2265023417c2de434b1",
+    "url": "/movie_app/static/css/main.e79ebd46.chunk.css"
+  },
+  {
+    "revision": "edad6c1d0544ed23b405eda4b906414e",
+    "url": "/movie_app/index.html"
+  }
+];
